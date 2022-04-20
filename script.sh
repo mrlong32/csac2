b4f2eeb34eb3f56e5e27b3f7370f087e137eb51d
mrlong32 <39040869+mrlong32@users.noreply.github.com>
2022-04-20 09:15:27 +0800

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# clone openwrt
+
+git clone -b openwrt-21.02 https://git.openwrt.org/openwrt/openwrt.git
+
+# version replace
+
+cd openwrt && sed -i 's/-SNAPSHOT/.1/g' include/version.mk
+
+# clone openwrt plugin source
+
+./scripts/feeds update -a && ./scripts/feeds install -a
+
+git clone https://github.com/gw826943555/openwrt-vlmcsd.git package/openwrt-vlmcsd
+
+# copy build file and config
+
+cp ../.config .config
+
+# openwrt build dependencies
+
+make defconfig && make download -j8
+
+# make openwrt source
+
+make -j4
